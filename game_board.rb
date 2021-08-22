66086f7b78c0a9734ae0c90df6eb9e76866b23d2
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-22 11:58:35 -0500

    Created a method to display the game board

--- game_board.rb
+++ game_board.rb
@@ -1,9 +1,23 @@
 class GameBoard
+  attr_reader :game_board
+
   def initialize
-    @game_board = Array.new(9, " _ ")
+    @game_board = (1..9).to_a
   end
 
   def update_board(player, move)
-    @game_board
+    game_board
+  end
+
+  def display_board
+    puts <<-HEREDOC
+
+          #{game_board[0]} | #{game_board[1]} | #{game_board[2]}
+         ---+---+---
+          #{game_board[3]} | #{game_board[4]} | #{game_board[5]}
+         ---+---+---
+          #{game_board[6]} | #{game_board[7]} | #{game_board[8]}
+          
+    HEREDOC
   end
 end
